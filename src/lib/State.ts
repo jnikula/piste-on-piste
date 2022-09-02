2b7934d9135677166cf6486bf2b708ac737a997b
Jani Nikula <jani@nikula.org>
2022-09-02 20:30:13 +0300

    state: fix typescript check

--- src/lib/State.ts
+++ src/lib/State.ts
@@ -3,7 +3,7 @@
 
 import Player from './Player';
 
-const MAX_BALLS = 15 + 6;
+const MAX_BALLS: number = 15 + 6;
 
 const permutations = [
   [0, 1, 2],
@@ -15,45 +15,25 @@ const permutations = [
 ]
 
 class State {
-  _initialize(names) {
-    // game
-    this.cur_perm = 0; // FIXME: start random
-    this.num_frames = 0;
+  // game
+  cur_perm: number = 0;
+  num_frames: number = 0;
 
-    // frame
-    this.timestamp = Date.now();
-    this.turn_timestamp = this.timestamp;
-    this._num_balls = MAX_BALLS;
+  // frame
+  timestamp: number;
+  turn_timestamp: number;
+  _num_balls: number = MAX_BALLS;
 
-    this.cur_pos = 0;
-    this.cur_pid = 0;
-    this.prev_pid = -1;
-    this.red = false;
-    this.foul = false;
-    this.retake = false;
+  cur_pos: number = 0;
+  cur_pid: number = 0;
+  prev_pid: number = -1;
+  red: boolean = false;
+  foul: boolean = false;
+  retake: boolean = false;
 
-    this.players = []
-    for (let pid of [0,1,2]) {
-      let name;
+  players: Player[] = [];
 
-      if (names && pid < names.length && names[pid].name && names[pid].name.length > 0)
-	name = names[pid].name;
-      else
-	name = `player ${pid}`;
-
-      let pos = permutations[this.cur_perm].indexOf(pid);
-
-      if (pos === this.cur_pos)
-	this.cur_pid = pid;
-
-      let p = new Player(pid, pos, name);
-
-      this.players.push(p)
-    }
-
-  }
-
-  _copy(source) {
+  _copy(source: Object): void {
     // this will override players
     Object.assign(this, source);
 
@@ -61,19 +41,40 @@ class State {
       this.players[i] = new Player(0, 0, '', this.players[i]);
   }
 
-  constructor(names=null, source=null) {
-    this._initialize(names);
+  constructor(names=null, source: Object = null) {
+    // frame
+    this.timestamp = Date.now();
+    this.turn_timestamp = this.timestamp;
+
+    for (let pid of [0,1,2]) {
+      let name: string;
+
+      if (names && pid < names.length && names[pid].name && names[pid].name.length > 0)
+	name = names[pid].name;
+      else
+	name = `player ${pid}`;
+
+      let pos: number = permutations[this.cur_perm].indexOf(pid);
+
+      if (pos === this.cur_pos)
+	this.cur_pid = pid;
+
+      let p: Player = new Player(pid, pos, name);
+
+      this.players.push(p)
+    }
+
     if (source)
       this._copy(source);
   }
 
-  deepcopy() {
-    let source = JSON.parse(JSON.stringify(this));
+  deepcopy(): State {
+    let source: Object = JSON.parse(JSON.stringify(this));
 
     return new State(null, source);
   }
 
-  _get_player_by_pid(pid) {
+  _get_player_by_pid(pid: number): Player {
     for (let p of this.players) {
       if (p.pid == pid)
 	return p;
@@ -84,7 +85,7 @@ class State {
     return null;
   }
 
-  _get_player_by_pos(pos) {
+  _get_player_by_pos(pos: number): Player {
     for (let p of this.players) {
       if (p.pos === pos)
 	return p;
@@ -95,30 +96,30 @@ class State {
     return null;
   }
 
-  previous_player() {
+  previous_player(): Player {
     return this._get_player_by_pid(this.prev_pid);
   }
 
-  current_player() {
+  current_player(): Player {
     return this._get_player_by_pid(this.cur_pid);
   }
 
   // All the statuses
-  num_reds() {
+  num_reds(): number {
     if (this._num_balls > 6)
       return this._num_balls - 6;
     else
       return 0
   }
 
-  num_colors() {
+  num_colors(): number {
     if (this._num_balls > 6)
       return 6;
     else
       return this._num_balls;
   }
 
-  num_balls(value) {
+  num_balls(value: number): number {
     if (value === 0)
       return this._num_balls;
     else if (value === 1)
@@ -129,34 +130,34 @@ class State {
       return 0;
   }
 
-  num_points() {
+  num_points(): number {
     if (this.num_colors() === 0)
       return 0;
 
-    const reds_and_blacks = this.num_reds() * (1 + 7);
-    const colors = [2, 3, 4, 5, 6, 7].slice(-this.num_colors()).reduce((a, b) => a + b);
+    const reds_and_blacks: number = this.num_reds() * (1 + 7);
+    const colors: number = [2, 3, 4, 5, 6, 7].slice(-this.num_colors()).reduce((a, b) => a + b);
 
     return reds_and_blacks + colors;
   }
 
-  get_players() {
+  get_players(): Player[] {
     return [...this.players].sort((p1, p2) => p1.pos - p2.pos);
   }
 
   // All the actions
 
-  can_concede(pid) {
+  can_concede(pid: number): boolean {
     if (this._is_frame_over())
       return false;
 
-    let p = this._get_player_by_pid(pid);
+    let p: Player = this._get_player_by_pid(pid);
 
     // current player can't concede
     if (this.is_current_player(pid))
       return false;
 
     // players still in the game
-    const players = this.players.filter((p) => !p.loser && !p.winner).sort((p1, p2) => p1.compare(p2));
+    const players: Player[] = this.players.filter((p) => !p.loser && !p.winner).sort((p1, p2) => p1.compare(p2));
 
     // only the last player can concede
     if (players[0].points != p.points)
@@ -169,23 +170,23 @@ class State {
     return true;
   }
 
-  concede(pid) {
+  concede(pid: number): void {
     let p = this._get_player_by_pid(pid);
     p.loser = true;
   }
 
-  can_declare_winner(pid) {
+  can_declare_winner(pid: number): boolean {
     if (this._is_frame_over())
       return false;
 
-    let p = this._get_player_by_pid(pid);
+    let p: Player = this._get_player_by_pid(pid);
 
     // current player can't be declared winner
     if (this.is_current_player(pid))
       return false;
 
     // players still in the game
-    const players = this.players.filter((p) => !p.loser && !p.winner).sort((p1, p2) => p1.compare(p2));
+    const players: Player[] = this.players.filter((p) => !p.loser && !p.winner).sort((p1, p2) => p1.compare(p2));
 
     // can declare winner only if everyone still in the game
     if (players.length != 3)
@@ -202,51 +203,53 @@ class State {
     return true;
   }
 
-  declare_winner(pid) {
-    let p = this._get_player_by_pid(pid);
+  declare_winner(pid: number): void {
+    let p: Player = this._get_player_by_pid(pid);
     p.winner = true;
   }
 
-  is_current_player(pid) {
+  is_current_player(pid: number): boolean {
     if (this._is_frame_over())
       return false;
 
-    let p = this.current_player();
+    let p: Player = this.current_player();
+
     return pid === p.pid;
   }
 
-  is_previous_player(pid) {
+  is_previous_player(pid: number): boolean {
     if (this._is_frame_over())
       return false;
 
-    let p = this.previous_player();
+    let p: Player = this.previous_player();
+
     return pid === p.pid;
   }
 
-  _out_of_reach(p1, p2) {
+  _out_of_reach(p1: Player, p2: Player): boolean {
     if (this.num_colors() > 1)
       return false;
 
     return Math.abs(p1.points - p2.points) > this.num_points();
   }
 
-  _num_players_left() {
+  _num_players_left(): number {
     return this.players.filter((p) => !p.loser && !p.winner).length;
   }
 
-  _have_winner() {
+  _have_winner(): boolean {
     return this.players.filter((p) => p.winner).length > 0;
   }
 
-  _have_loser() {
+  _have_loser(): boolean {
     return this.players.filter((p) => p.loser).length > 0;
   }
 
-  _is_frame_over() {
+  _is_frame_over(): boolean {
     return this._have_winner() && this._have_loser();
   }
 
-  _detect_win_lose() {
+  _detect_win_lose(): void {
     // players still in the game
     const players = this.players.filter((p) => !p.loser && !p.winner).sort((p1, p2) => p1.compare(p2));
 
@@ -266,11 +269,11 @@ class State {
     }
   }
 
-  _sorted_players() {
+  _sorted_players(): Player[] {
     return [...this.players].sort((p1, p2) => p1.compare(p2));
   }
 
-  _log_player_time() {
+  _log_player_time(): void {
     const now = Date.now();
     const turn_duration = now - this.turn_timestamp;
 
@@ -280,13 +283,13 @@ class State {
     p.log_time(turn_duration);
   }
 
-  _end_turn() {
+  _end_turn(): void {
     this._log_player_time();
 
-    let p = this.current_player();
+    let p: Player = this.current_player();
     p.end_turn();
 
-    let retake = this.retake;
+    let retake: boolean = this.retake;
 
     this.prev_pid = this.cur_pid;
     this.red = false;
@@ -314,7 +317,7 @@ class State {
       p = this._get_player_by_pos(this.cur_pos);
       this.cur_pid = p.pid;
     } else if (this._num_players_left() === 2) {
-      let player;
+      let player: Player;
 
       while (true) {
 	this.cur_pos++;
@@ -336,10 +339,10 @@ class State {
     }
   }
 
-  _pot_points(points) {
+  _pot_points(points: number): void {
     this.foul = false;
 
-    let p = this.current_player();
+    let p: Player = this.current_player();
     p.pot_points(points);
 
     // game over
@@ -347,12 +350,12 @@ class State {
       this._end_turn();
   }
 
-  _can_pot_red() {
+  _can_pot_red(): boolean {
     // Note: Can pot two reds at a time!
     return this.num_reds() > 0;
   }
 
-  _pot_red() {
+  _pot_red(): void {
     console.assert(this._can_pot_red());
 
     this._num_balls--;
@@ -361,7 +364,7 @@ class State {
     this._pot_points(1);
   }
 
-  _can_pot_color(value) {
+  _can_pot_color(value: number): boolean {
     if (this.red)
       return true;
 
@@ -371,7 +374,7 @@ class State {
     return this.num_colors() - (7 - value + 1) === 0;
   }
 
-  _pot_color(value) {
+  _pot_color(value: number): void {
     console.assert(this._can_pot_color(value));
 
     if (this.num_reds() === 0 && !this.red)
@@ -382,14 +385,14 @@ class State {
     this._pot_points(value);
   }
 
-  can_pot_ball(value) {
+  can_pot_ball(value: number): boolean {
     if (value === 1)
       return this._can_pot_red();
     else
       return this._can_pot_color(value);
   }
 
-  pot_ball(value) {
+  pot_ball(value: number): void {
     console.log('pot ball: ' + value)
 
     if (value === 1)
@@ -398,14 +401,14 @@ class State {
       this._pot_color(value);
   }
 
-  can_commit_foul(value) {
+  can_commit_foul(value: number): boolean {
     return this.num_colors() - (7 - value + 1) >= 0;
   }
 
-  commit_foul(value) {
+  commit_foul(value: number): void {
     console.assert(this.can_commit_foul(value));
 
-    let player = this.current_player();
+    let player: Player = this.current_player();
     if (this.prev_pid === -1 ||
 	player._cur_break.length > 0 ||
 	this.retake ||
@@ -414,7 +417,7 @@ class State {
 	if (p.pid != this.cur_pid)
 	  p.points += value;
     } else {
-      let p = this.previous_player();
+      let p: Player = this.previous_player();
       p.points += value;
     }
 
@@ -425,15 +428,15 @@ class State {
     this.foul = true;
   }
 
-  can_end_turn() {
+  can_end_turn(): boolean {
     return !this._is_frame_over();
   }
 
-  end_turn() {
+  end_turn(): void {
     this._end_turn();
   }
 
-  _end_frame() {
+  _end_frame(): void {
     this.num_frames++;
 
     for (let p of this.players) {
@@ -446,11 +449,11 @@ class State {
     }
   }
 
-  can_new_frame() {
+  can_new_frame(): boolean {
     return this._is_frame_over();
   }
 
-  new_frame() {
+  new_frame(): void {
     // FIXME: this should happen earlier, when frame is over
     this._end_frame();
 
@@ -472,7 +475,7 @@ class State {
     this.retake = false;
 
     for (let p of this.players) {
-      let pos = permutations[this.cur_perm].indexOf(p.pid);
+      let pos: number = permutations[this.cur_perm].indexOf(p.pid);
 
       p.new_frame(pos);
 
@@ -481,33 +484,33 @@ class State {
     }
   }
 
-  can_plus_balls() {
+  can_plus_balls(): boolean {
     return this._num_balls < MAX_BALLS;
   }
 
-  plus_balls() {
+  plus_balls(): void {
     console.log('plus balls');
     console.assert(this.can_plus_balls());
 
     this._num_balls++;
   }
 
-  can_minus_balls() {
+  can_minus_balls(): boolean {
     return this._num_balls > 0;
   }
 
-  minus_balls() {
+  minus_balls(): void {
     console.log('minus balls');
     console.assert(this.can_minus_balls());
 
     this._num_balls--;
   }
 
-  can_foul_retake() {
+  can_foul_retake(): boolean {
     return this.foul && !this._is_frame_over();
   }
 
-  foul_retake() {
+  foul_retake(): void {
     console.assert(this.can_foul_retake());
 
     this._log_player_time()

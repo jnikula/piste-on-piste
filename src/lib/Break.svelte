3d206756ca6c424588a792d2b5b6e5ecfd7f61f5
Jani Nikula <jani@nikula.org>
2024-02-12 00:05:22 +0200

    break: fix break display for lots of balls
    For breaks more than 10 balls, switch to showing ball counts. This is a
    bit like TV broadcast display of the balls potted.
    
    Unfortunately a lot of fonts appear to have size variations in the
    numbers, because 0 and 11-15 come from different unicode blocks. Anyway,
    this is a start, may be improved later.

--- src/lib/Break.svelte
+++ src/lib/Break.svelte
@@ -3,11 +3,50 @@
 <script lang='ts'>
   export let balls: number[];
   import { value_to_csscolor } from './ball-colors';
+
+  let counts: number[];
+  let err: number;
+
+  $: ball_counts = balls.length > 10;
+
+  $: {
+    counts = [0,0,0,0,0,0,0,0];
+    err = 0;
+
+    for (let i of balls) {
+      if (i > 0)
+	counts[i]++;
+      else
+	err = i;
+    }
+  }
+
+  function count_to_char(count: number): string {
+    // 1-10 Dingbats, 0, 11-15 Enclosed Alphanumerics *may* have different
+    // sizes...
+    let count_chars: string[] = [
+      '⓿', '❶', '❷', '❸', '❹', '❺', '❻', '❼', '❽', '❾', '❿',
+      '⓫', '⓬', '⓭', '⓮', '⓯'
+    ];
+
+    return count_chars[count];
+  }
 </script>
 
-{#each balls as value}
-  {value < 0 ? '-' : ''}<span class='ball' style='--csscolor: {value_to_csscolor(value)};'>&#x2B24;</span>
-{/each}
+{#if ball_counts}
+  {#each counts as count, value}
+    {#if count > 0}
+      <span class='ball' style='--csscolor: {value_to_csscolor(value)};'>{count_to_char(count)}</span>
+    {/if}
+  {/each}
+  {#if err < 0}
+    -<span class='ball' style='--csscolor: {value_to_csscolor(err)};'>{count_to_char(1)}</span>
+  {/if}
+{:else}
+  {#each balls as value}
+    {value < 0 ? '-' : ''}<span class='ball' style='--csscolor: {value_to_csscolor(value)};'>&#x2B24;</span>
+  {/each}
+{/if}
 
 <style>
   .ball {

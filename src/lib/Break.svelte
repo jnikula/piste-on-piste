28911f2f08fab4cf2967683c394bb7530d3a4223
Jani Nikula <jani@nikula.org>
2024-03-02 22:41:36 +0200

    break: draw break balls using css not characters
    Use CSS to draw the break balls around the values instead of using
    unicode chars with the discs.

--- src/lib/Break.svelte
+++ src/lib/Break.svelte
@@ -7,7 +7,7 @@
   let counts: number[];
   let err: number;
 
-  $: ball_counts = balls.length > 10;
+  $: ball_counts = balls.length > 8;
 
   $: {
     counts = [0,0,0,0,0,0,0,0];
@@ -20,40 +20,38 @@
 	err = i;
     }
   }
-
-  function count_to_char(count: number): string {
-    // 1-10 Dingbats, 0, 11-15 Enclosed Alphanumerics *may* have different
-    // sizes...
-    let count_chars: string[] = [
-      '⓿', '❶', '❷', '❸', '❹', '❺', '❻', '❼', '❽', '❾', '❿',
-      '⓫', '⓬', '⓭', '⓮', '⓯'
-    ];
-
-    return count_chars[count];
-  }
 </script>
 
 {#if ball_counts}
   {#each counts as count, value}
     {#if count > 0}
-      <span class='ball' style='--csscolor: {value_to_csscolor(value)};'>{count_to_char(count)}</span>
+      <span class='ball value' style='--csscolor: {value_to_csscolor(value)};'>{count}</span>
     {/if}
   {/each}
-  {#if err < 0}
-    -<span class='ball' style='--csscolor: {value_to_csscolor(err)};'>{count_to_char(1)}</span>
-  {/if}
 {:else}
   {#each balls as value}
-    {value < 0 ? '-' : ''}<span class='ball' style='--csscolor: {value_to_csscolor(value)};'>&#x2B24;</span>
+    {#if value > 0}
+      <span class='ball plain' style='--csscolor: {value_to_csscolor(value)};'>.</span>
+    {/if}
   {/each}
+{/if}
+{#if err < 0}
+  <span class='ball value' style='--csscolor: {value_to_csscolor(err)};'>F</span>
 {/if}
 
 <style>
   .ball {
     background-image: radial-gradient(circle at 66% 25%, white, var(--csscolor), black);
-    background-clip: text;
-    -webkit-background-clip: text;
-    color: transparent;
-    font-size: 80%;
+    border-radius: 50%;
+    text-align: center;
+    display: inline-block;
+    color: transparent; /* hide the contents */
+    line-height: 1.0em;
+    width: 1.0em;
+    margin: 0.1em;
+  }
+  .value {
+    text-shadow: 1px 1px black;
+    color: white;
   }
 </style>
